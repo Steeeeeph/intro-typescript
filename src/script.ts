b49e8acf16baa3b85a3f874a90e3fb552b209161
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2021-02-22 13:55:11 +0100

    Error-free code

--- src/script.ts
+++ src/script.ts
@@ -1,36 +1,39 @@
-"use strict";
-(function () {
-    const inputForm = document.getElementById('guess-form');
+(function() {
+    const  inputForm = document.getElementById('guess-form')!;
     const min = 1;
     const max = 22;
+
     if (inputForm === null) {
         return alert("Please enter a number between 1 and 22!");
+    } else {
+    inputForm.addEventListener("submit", compareUserGuess);
     }
-    else {
-        inputForm.addEventListener("submit", compareUserGuess);
-    }
-    function compareUserGuess(event) {
+    function compareUserGuess (event: any) {
         event.preventDefault();
-        const secretNumber = generateSecretNumber();
-        const userNumberPath = document.getElementById('guess');
-        const userNumber = Number(userNumberPath.value);
+
+        const secretNumber: number = generateSecretNumber();
+        const userNumberPath = <HTMLInputElement>document.getElementById('guess')!;
+        const userNumber: number = Number(userNumberPath.value);
+
         if (userNumberPath === null) {
             return alert("Please enter a number between 1 and 22!");
-        }
-        else {
+        } else {
             if (secretNumber == userNumber) {
                 alert("Awesome! You number " + userNumber + " was correct. You can be named many things, hungry not being one of them.");
-            }
-            else {
+            } else {
                 alert("Bummer... You guessed " + userNumber + " and the secret number was " + secretNumber + ".");
-            }
+            }    
         }
+
     }
+
     function generateSecretNumber() {
         const secretNumber = getRandomArbitrary(1, 22);
+
         return secretNumber;
     }
-    function getRandomArbitrary(min, max) {
+
+    function getRandomArbitrary(min: number, max: number) {
         return Math.round(Math.random() * (max - min) + min);
     }
 })();
