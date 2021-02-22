76379a044dc73a2d2d834afb7967c1fe1454f70d
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2021-02-22 10:37:55 +0100

    Revert "Adding config file, allowing JS to be compiled and changing js-dir in the index"
    This reverts commit 3c9f1a97b4c76b76ae957cbd02e780448521d878.

--- src/script.js
+++ src/script.js
@@ -1,20 +1,25 @@
-(function () {
+(function() {
     document.getElementById('guess-form').addEventListener("submit", compareUserGuess);
-    function compareUserGuess(event) {
+
+    function compareUserGuess (event) {
         event.preventDefault();
-        var secretNumber = generateSecretNumber();
-        var userNumber = document.getElementById('guess').value;
+
+        const secretNumber = generateSecretNumber();
+        const userNumber = document.getElementById('guess').value;
+
         if (secretNumber == userNumber) {
             alert("Awesome! You number " + userNumber + " was correct. You can be named many things, hungry not being one of them.");
-        }
-        else {
+        } else {
             alert("Bummer... You guessed " + userNumber + " and the secret number was " + secretNumber + ".");
         }
     }
+
     function generateSecretNumber() {
-        var secretNumber = getRandomArbitrary(1, 22);
+        const secretNumber = getRandomArbitrary(1, 22);
+
         return secretNumber;
     }
+
     function getRandomArbitrary(min, max) {
         return Math.round(Math.random() * (max - min) + min);
     }
